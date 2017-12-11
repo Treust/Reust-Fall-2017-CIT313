f824a91f17331f815b4b53489200b0d466514505
Treust <31217372+Treust@users.noreply.github.com>
2017-12-10 20:03:45 -0500

    comments on else iff

--- final/application/controllers/logincontroller.php
+++ final/application/controllers/logincontroller.php
@@ -8,43 +8,37 @@ class LoginController extends Controller{
 	   // handle login
 
        $this->userObject = new Users();
-
+       //if test 1 check to see if login/password match
        if($this->userObject->checkUser($_POST['email'],$_POST['password'])) {
 
            $userInfo = $this->userObject->getUserFromEmail($_POST['email']);
 
            $_SESSION['uID'] = $userInfo['uID'];
 
+              //if test 2 check if user is active
+              if($userinfo->isActive()) {
 
-
-           if($userinfo->isActive()) {
-
-
-          if(strlen($_SESSION['redirect']) > 0 ) {
-              $view = $_SESSION['redirect'];
-              unset($_SESSION['redirect']);
-              header('Location: '.BASE_URL.$view);
-          }
-           else {
-               header('Location: '.BASE_URL);
-           }
-
-           else {
-               $this->set('error','awaiting admin approval');
-             }
-
-
+                  //if test 3
+                  if(strlen($_SESSION['redirect']) > 0 ) {
+                      $view = $_SESSION['redirect'];
+                      unset($_SESSION['redirect']);
+                      header('Location: '.BASE_URL.$view);
+                    }
+                    //else for test 3
+                    else {
+                      header('Location: '.BASE_URL);
+                    }
+              //else test 2
+              else {
+                $this->set('error','awaiting admin approval');
+                }
+        //else test 1
        else {
            $this->set('error','Wrong password / email combination');
+            }
        }
      }
    }
-}
-
-
-
-
-
 
 
     public function logout() {
